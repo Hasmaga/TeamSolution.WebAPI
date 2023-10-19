e43908a99c99403e3fb82f48559db5fcc185a45b
Dangkhoa305 <dangkhoa1699@gmail.com>
2023-10-19 17:00:11 +0700

    add Issuse Service

--- TeamSolution/Service/IssueService.cs
+++ TeamSolution/Service/IssueService.cs
@@ -1,8 +1,15 @@
-﻿using TeamSolution.Service.Interface;
+﻿using TeamSolution.Repository;
+using TeamSolution.Service.Interface;
 
 namespace TeamSolution.Service
 {
     public class IssueService : IIssueService
     {
+        private IssueRepository _repository;
+        public IssueService (IssueRepository repository)
+        {
+            _repository = repository;
+        }
+
     }
 }
