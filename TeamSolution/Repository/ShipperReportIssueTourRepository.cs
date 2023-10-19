a589e9fbe30bb667c69c73e1ffdbf474953fdf5e
KhangBPA <khangbpak2001@gmail.com>
2023-10-19 22:41:54 +0700

    Update

--- TeamSolution/Repository/ShipperReportIssueTourRepository.cs
+++ TeamSolution/Repository/ShipperReportIssueTourRepository.cs
@@ -1,8 +1,14 @@
-﻿using TeamSolution.Repository.Interface;
+﻿using TeamSolution.DatabaseContext;
+using TeamSolution.Repository.Interface;
 
 namespace TeamSolution.Repository
 {
     public class ShipperReportIssueTourRepository : IShipperReportIssueTourRepository
     { 
+        private readonly ApplicationDbContext _context;
+        public ShipperReportIssueTourRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
