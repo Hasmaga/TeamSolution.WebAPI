e754bcd700efecfdfec65c04556abb3b05db6a06
KhangBPA <khangbpak2001@gmail.com>
2023-10-19 22:44:14 +0700

    Update

--- TeamSolution/Repository/TourShipperRepository.cs
+++ TeamSolution/Repository/TourShipperRepository.cs
@@ -1,8 +1,14 @@
-﻿using TeamSolution.Repository.Interface;
+﻿using TeamSolution.DatabaseContext;
+using TeamSolution.Repository.Interface;
 
 namespace TeamSolution.Repository
 {
     public class TourShipperRepository : ITourShipperRepository
     {
+        private readonly ApplicationDbContext _context;
+        public TourShipperRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
