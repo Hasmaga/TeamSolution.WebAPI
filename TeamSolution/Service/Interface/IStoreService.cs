3fb0bf22726b12409c81c5bd76b0d0cf27238ff0
bachdoan-sa <bach.doansy@gmail.com>
2023-10-20 23:26:25 +0700

    add Method create Store

--- TeamSolution/Service/Interface/IStoreService.cs
+++ TeamSolution/Service/Interface/IStoreService.cs
@@ -1,9 +1,12 @@
 ﻿using TeamSolution.Model;
+using TeamSolution.ViewModel.Store;
 
 namespace TeamSolution.Service.Interface 
 {
     public interface IStoreService
     {
         Task<Store> GetAllStore();
+        Task<ICollection<Store>> GetAllStoreEntity();
+        Task<Guid> CreateStoreAsync(StoreModel store);
     }
 }
