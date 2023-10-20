6707ca02008493c5c5d5083da8d0ce72c137aa0d
bachdoan-sa <bach.doansy@gmail.com>
2023-10-20 23:25:49 +0700

    add viewmodel

---
+++ TeamSolution/ViewModel/Store/StoreModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamSolution.ViewModel.Store
+{
+    public class StoreModel
+    {
+        public string StoreName { get; set; }
+        public string Address { get; set; }
+        public string? StoreDescription { get; set; }
+        public string Phone { get; set; }
+        public string? StoreImage { get; set; }
+        public Guid StoreManagerId { get; set; }
+        public string? OperationTime { get; set; }
+    }
+}
