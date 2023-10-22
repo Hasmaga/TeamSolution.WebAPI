3dbd9625d1a1b69b4c047fa6e63aa594760a473b
bachdoan-sa <bach.doansy@gmail.com>
2023-10-22 12:29:04 +0700

    add Enum Code

--- TeamSolution/Enum/SucessfulCode.cs
+++ TeamSolution/Enum/SucessfulCode.cs
@@ -6,5 +6,11 @@
         public const string CREATE_ROLE_SUCCESSFULLY = "CREATE_ROLE_SUCCESSFULLY";
         public const string VALIDATE_ACCOUNT_SUCCESSFULLY = "VALIDATE_ACCOUNT_SUCCESSFULLY";
         public const string CREATE_ORDER_SUCCESSFULLY = "CREATE_ORDER_SUCCESSFULLY";
+
+        #region StoreConstants
+        public const string CREATE_STORE_SUCCESSFULLY = "CREATE_STORE_SUCCESSFULLY";
+        public const string UPDATE_STORE_SUCCESSFULLY = "UPDATE_STORE_SUCCESSFULLY";
+        #endregion
+
     }
 }
