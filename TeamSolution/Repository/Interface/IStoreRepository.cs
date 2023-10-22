a566afe18badad949d6be395f26b062c422d924f
bachdoan-sa <bach.doansy@gmail.com>
2023-10-22 12:28:49 +0700

    add Method Read and Update for Store

--- TeamSolution/Repository/Interface/IStoreRepository.cs
+++ TeamSolution/Repository/Interface/IStoreRepository.cs
@@ -1,4 +1,5 @@
 ﻿using TeamSolution.Model;
+using TeamSolution.ViewModel.Store;
 
 namespace TeamSolution.Repository.Interface
 {
@@ -7,5 +8,6 @@ namespace TeamSolution.Repository.Interface
         Task<Store> GetStoreByIdRepositoryAsync(Guid id);
         Task<ICollection<Store>> GetAll(bool includeIsDeleted = false);
         Task<Guid> CreateAsync(Store store, CancellationToken cancellationToken = default);
+        Task<Guid> UpdateAsync(UpdateStoreRequestModel updateStoreRequest, CancellationToken cancellationToken = default);
     }
 }
